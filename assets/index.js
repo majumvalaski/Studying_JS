8bf030218733754aa42c136c41fb1032a3cbfe0e
Maria Julia Valaski <maju.m.valaski@gmail.com>
2023-01-24 10:37:20 -0200

    concatenation string

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,17 @@
 console.log('it works');
 const name = 'blue';
 const middle = "topher";
-//const last = `silva`;
+const last = `silva`;
+
+const sentence = "shes's so \"cool\"";
+const sentence2 = `she\`s's so "cool"`;
+
+const html = `
+  <div>
+    <h2></h2>
+  </div>
+`;
+
+const hello = "hello my name is. Nice to meet you";
+let name1 = "claudio";
+const hello1 = "hello my name is " + name1 + ". Nice to meet you.";
