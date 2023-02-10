04ade21fd15197c46b31689adc16b87e8de668b0
Maria Julia Valaski <maju.m.valaski@gmail.com>
2023-02-10 10:10:43 -0200

    using backsticks

--- assets/index.js
+++ assets/index.js
@@ -15,3 +15,13 @@ const html = `
 const hello = "hello my name is. Nice to meet you";
 let name1 = "claudio";
 const hello1 = "hello my name is " + name1 + ". Nice to meet you.";
+
+
+const name2 = 'Wes';
+const hello2 = `Hello my name is ${name2}. Nice to meet you. I am ${40 +38} years old`;
+const html1 = `
+  <div>
+    <h2>${name2}</h2>
+    <p>${hello2}</p>
+  </div>  
+`;
